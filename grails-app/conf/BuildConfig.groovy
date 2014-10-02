a36b37423e45bf14089479a7175ac3bf34495afb
valentinsvt <valentinsvt@hotmail.com>
2014-10-02 11:23:56 -0500

    Initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,39 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.dependency.resolution = {
+    grails.project.mavenCache = "C:\\Users\\svt\\Documents\\GitHub\\happy"
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        // runtime 'mysql:mysql-connector-java:5.1.24'
+    }
+
+    plugins {
+        build(":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
+
+        }
+        compile ":hibernate:2.1.5" // or ":hibernate4:4.1.11.6"
+
+    }
+}
+
